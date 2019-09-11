bcbb74f4d160c7e7581ffc58a2836f5f6c4ff217
Dante Spencer <spencerdantel@gmail.com>
2019-09-11 16:07:32 +0000

    adding elements and classes

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,3 +5,25 @@ require "pry"
 html = open("https://www.fws.gov/endangered/news/index.html")
 doc = Nokogiri::HTML(html)
 binding.pry
+
+#articles: doc.css("div.article")
+#title: doc.css("h2").text
+#date: doc.css("p.dateline").text
+
+class E_Species
+  @@all = []
+  attr_accessor :title, :date
+  
+  def initialize (title,date)
+  @title = title
+  @date = date
+  @@all << self
+end
+
+def self.all
+  @@all
+end
+
+end
+
+
