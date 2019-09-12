f3d837518eb655c00e2c1ca3cee5d4dda13a9e7f
Dante Spencer <spencerdantel@gmail.com>
2019-09-12 14:27:38 +0000

    scraper updates

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -31,7 +31,7 @@ doc = Nokogiri::HTML(html)
 article_elements = doc.css("div.article")
 
 
-
+# article_el is the the actual article element (1 of 12) in the array. The css in this method is working on it just as we defined above when pulling specific classes
 article_elements.each do |article_el|
   Article.new(
     article_el.css("h2").text, 
@@ -39,6 +39,15 @@ article_elements.each do |article_el|
     article_el.css("a").attr('href').value
     )
   end
+  
+  #In the list of articles array, opens page to provided url for scraping
+  Article.all.each do |article|
+    doc = Nokogiri::HTML(open(article.url))
+    #add method to the Article class that represents a variable for some element scraped on this page
+    #Call the method (Article.whatever) and set it equal to the element on this page
+    binding.pry
+  end
+  
 
   
 binding.pry
