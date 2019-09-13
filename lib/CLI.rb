78cf6909e1b6f22f25062d979c43f8f540f74425
Dante Spencer <spencerdantel@gmail.com>
2019-09-13 19:05:32 +0000

    added looping and breaks

--- lib/CLI.rb
+++ lib/CLI.rb
@@ -8,27 +8,44 @@ class CLI
     list_articles
 # Runs scraper of initial site that returns list of articles (.scrape_article_list method). Assigned to .call method. First puts some text. Returns a list of articles with list_articles variable (see below).
     
-    puts 'Would You Like to Select an Aritcle Number for More Info? y/n'
+   # puts 'Would You Like to Select an Aritcle Number for More Info? y/n'
 # Asks for user input to choose an article    
-     input = gets.strip
-     if input == "y"
-     puts "Select Article Number"
-   elseif input = "n"
-     puts "Have a great day!" 
-     end
-     input2 = gets.strip
-   
-     
+     #input = gets.strip
+     loop do
+           puts 'Would You Like to Select an Aritcle Number for More Info? y/n'
+
+       input = gets.strip
+        if input == "n"
+         puts "Have a great day!" 
+         break
+        elsif input == "y"
+         puts "Select Article Number:"
+         input2 = gets.strip
+         article = Article.all[input2.to_i-1]
+         puts show_details(article)
+         sleep 5
+        list_articles
+                   puts 'Would You Like to Select an Aritcle Number for More Info? y/n'
+
+       else 
+        puts "Try again"
+       until input == "y" || "n"
+       end
      
     
+  
+   
 # Assigns a particular article choice from Article.all array (input from user to choose index of selected article) and assigns to 'article' variable    
     article = Article.all[input2.to_i-1]
+  end
 
 # Scrapes url page of selected article and returns subtitle   
     Scraper.scrape_article(article)
+end
+end
     
-    puts show_details(article)
-  end
+    #puts show_details(article)
+
     
     
     
