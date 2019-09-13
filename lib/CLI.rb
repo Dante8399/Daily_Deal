7ce729b34c13f7849c9f406168642a7a8eea01b5
Dante Spencer <spencerdantel@gmail.com>
2019-09-13 17:49:22 +0000

    updating CLI

--- lib/CLI.rb
+++ lib/CLI.rb
@@ -8,21 +8,30 @@ class CLI
     list_articles
 # Runs scraper of initial site that returns list of articles (.scrape_article_list method). Assigned to .call method. First puts some text. Returns a list of articles with list_articles variable (see below).
     
-    puts 'Select an Aritcle Number for More Info'
+    puts 'Would You Like to Select an Aritcle Number for More Info? y/n'
 # Asks for user input to choose an article    
-    input = gets.strip
+     input = gets.strip
+     if input == "y"
+     puts "Select Article Number"
+   elseif input = "n"
+     puts "Have a great day!" 
+     end
+     input2 = gets.strip
+   
+     
+     
     
 # Assigns a particular article choice from Article.all array (input from user to choose index of selected article) and assigns to 'article' variable    
-    article = Article.all[input.to_i-1]
+    article = Article.all[input2.to_i-1]
 
 # Scrapes url page of selected article and returns subtitle   
     Scraper.scrape_article(article)
     
     puts show_details(article)
-    
-    
-    
   end
+    
+    
+    
   
   def list_articles
 # Returns entire list of articles, with an index, and puts the index (+1) and the article title (from title method in Article class)    
